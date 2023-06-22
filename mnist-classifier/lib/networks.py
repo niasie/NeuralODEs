e31d0aff051db23d13288d9131b7345565171ff1
JoshuaAurand <aurandj@student.ethz.ch>
2023-06-22 18:20:21 +0200

    mnist classifier from computer vision course

---
+++ mnist-classifier/lib/networks.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+from torch.nn.modules.activation import ReLU
+
+
+class MLPClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.codename = 'mlp'
+
+        # Define the network layers in order.
+        # Input is 28 * 28.
+        # Output is 10 values (one per class).
+        # Multiple linear layers each followed by a ReLU non-linearity (apart from the last).
+        
+        # First Part: simple linear classifier
+        #self.layers = nn.Sequential(
+        #    nn.Linear(28 * 28, 10)
+        #)
+        
+        self.layers = nn.Sequential(
+            nn.Linear(28 * 28, 32),
+            nn.ReLU(),
+            nn.Linear(32, 10)
+        )
+    
+    def forward(self, batch):
+        # Flatten the batch for MLP.
+        b = batch.size(0)
+        batch = batch.view(b, -1)
+        # Process batch using the layers.
+        x = self.layers(batch)
+        return x
+
+
+class ConvClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.codename = 'conv'
+
+        # Define the network layers in order.
+        # Input is 28x28, with one channel.
+        # Multiple Conv2d and MaxPool2d layers each followed by a ReLU non-linearity (apart from the last).
+        # Needs to end with AdaptiveMaxPool2d(1) to reduce everything to a 1x1 image.
+        self.layers = nn.Sequential(
+            nn.Conv2d(1, 8, (3, 3)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2), 2),
+            nn.Conv2d(8, 16, (3, 3)),
+            nn.ReLU(),
+            nn.MaxPool2d((2, 2), 2),
+            nn.Conv2d(16, 32, (3, 3)),
+            nn.ReLU(),
+            nn.AdaptiveMaxPool2d(1)
+        )
+        # Linear classification layer.
+        # Output is 10 values (one per class).
+        self.classifier = nn.Sequential(
+            nn.Linear(32, 10) # one output per channel (each channel reduced to 1x1 by the pooling)
+        )
+    
+    def forward(self, batch):
+        # Add channel dimension for conv.
+        b = batch.size(0)
+        batch = batch.unsqueeze(1)
+        # Process batch using the layers.
+        x = self.layers(batch)
+        x = self.classifier(x.view(b, -1))
+        return x
