b7956f813ae34747f7e965fbd3f051420c60d57e
JoshuaAurand <aurandj@student.ethz.ch>
2023-06-25 17:43:48 +0200

    add with no grad to eval

--- utils.py
+++ utils.py
@@ -35,27 +35,28 @@ def train(
         loss_total /= n_batch
 
         writer.add_scalar("train_loss", loss_total, epoch)
+        
+        with torch.no_grad():
+            if validation_set is not None:
+                if epoch % validation_interval == 0:
+                    model.eval()
+                    val_score = validation_model(model, validation_set)
+                    writer.add_scalar("val_score", val_score, epoch)
 
-        if validation_set is not None:
-            if epoch % validation_interval == 0:
-                model.eval()
-                val_score = validation_model(model, validation_set)
-                writer.add_scalar("val_score", val_score, epoch)
+                    checkpoint = {
+                        "epoch_idx": epoch,
+                        "net": model.state_dict(),
+                        "optimizer": optimizer.state_dict(),
+                    }
 
-                checkpoint = {
-                    "epoch_idx": epoch,
-                    "net": model.state_dict(),
-                    "optimizer": optimizer.state_dict(),
-                }
-
-                if mode == "max":
-                    if val_score > val_best:
-                        val_best = val_score
-                        torch.save(checkpoint, checkpoint_path)
-                elif mode == "min":
-                    if val_score < val_best:
-                        val_best = val_score
-                        torch.save(checkpoint, checkpoint_path)
+                    if mode == "max":
+                        if val_score > val_best:
+                            val_best = val_score
+                            torch.save(checkpoint, checkpoint_path)
+                    elif mode == "min":
+                        if val_score < val_best:
+                            val_best = val_score
+                            torch.save(checkpoint, checkpoint_path)
 
 
 # https://discuss.pytorch.org/t/how-do-i-check-the-number-of-parameters-of-a-model/4325/23?page=2
